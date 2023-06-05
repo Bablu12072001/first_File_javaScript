97db46271135eb35b56a1fac9bb9e16eed91155a
Bablu12072001 <bkumar41366@gmail.com>
2023-06-05 08:33:25 +0530

    html

---
+++ dom.js
@@ -0,0 +1,28 @@
+// Examine the Document object
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 455;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = "Hello";
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.forms[0]);
+
+// GET ELEMENT BY ID
+// console.log(document.getElementById("header-title"));
+// var headerTitle = document.getElementById("header-title");
+// var header = document.getElementById("main-header");
+// console.log(headerTitle);
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "Goodbye";
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// header.style.borderBottom = "solid 3px black";
+
+// GET ELEMENT BY CLASS NAME
