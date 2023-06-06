e7156c4ebcef17be12eaeb1a8a799d4e3bb7b7a7
Bablu12072001 <bkumar41366@gmail.com>
2023-06-06 08:46:09 +0530

    getElementsByClassName

--- dom.js
+++ dom.js
@@ -26,3 +26,16 @@
 // header.style.borderBottom = "solid 3px black";
 
 // GET ELEMENT BY CLASS NAME
+
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
+console.log(items[1]);
+items[1].textContent = "Hello 1";
+items[1].style.fontWeight = "bold";
+items[1].style.backgroundColor = "yellow";
+
+// items.style.backgroundColor = "gray";
+
+for (var i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = "gray";
+}
