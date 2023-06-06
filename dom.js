1f12bad9e1cee7f0d05d119519185efa09a2cdcb
Bablu12072001 <bkumar41366@gmail.com>
2023-06-06 10:37:10 +0530

    GetElementByTag

--- dom.js
+++ dom.js
@@ -27,15 +27,33 @@
 
 // GET ELEMENT BY CLASS NAME
 
-var items = document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[1]);
-items[1].textContent = "Hello 1";
-items[1].style.fontWeight = "bold";
-items[1].style.backgroundColor = "yellow";
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = "Hello 1";
+// items[1].style.fontWeight = "bold";
+// items[1].style.backgroundColor = "yellow";
 
 // items.style.backgroundColor = "gray";
 
-for (var i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = "gray";
-}
+// for (var i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = "gray";
+// }
+
+// Get Element by class name
+// var li = document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = "Hello 1";
+// li[1].style.fontWeight = "bold";
+// li[1].style.backgroundColor = "yellow";
+
+// li.style.backgroundColor = "gray";
+
+// for (var i = 0; i < li.length; i++) {
+//   li[i].style.backgroundColor = "yellow";
+// }
+
+// QuerySelector
+var header = document.querySelector("#main-header");
+header.style.borderBottom = "solid 4px #ccc";
